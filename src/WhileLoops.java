a886cb1e1b36833970ec958e561991c3ad957026
mishgan55 <vorota.mogilev@gmail.com>
2023-05-18 16:40:00 +0300

    Loops While

---
+++ src/WhileLoops.java
@@ -0,0 +1,10 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int value=1;
+        String s="Hello ";
+        while (value<=5){
+            System.out.println(s+value);
+            value=value+1;
+        }
+    }
+}
