ae0d23a0168179a4c5178d67a9c9f1db242d38d6
mishgan55 <vorota.mogilev@gmail.com>
2023-05-18 16:22:51 +0300

    String

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x=55;
+        String s="Hello";
+        String space=" ";
+        String name="John";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"Misha");
+        System.out.println("My number "+x);
+    }
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Variables {
+public class Variables {
+    public static void main(String[] args) {
+        int myInt =557;
+        short myShort=23123;
+        long myLong=123124343;
+
+        double myDouble=123.2;
+        float myFloat=234.5f;
+        char c='a';
+        boolean b=true;
+        byte myByte=127;
+
+        System.out.println(myInt);
+    }
 }
