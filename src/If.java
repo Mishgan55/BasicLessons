8b75957d071cfdb159dc74d5c97354c5f7ba7e63
mishgan55 <vorota.mogilev@gmail.com>
2023-05-19 16:23:35 +0300

    If

---
+++ src/If.java
@@ -0,0 +1,13 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt=50;
+        if (myInt>70){
+            System.out.println("Верно");
+        }else if (myInt<40){
+            System.out.println("Не верно");
+
+        }else {
+            System.out.println("Ни одно из этих условий");
+        }
+    }
+}
