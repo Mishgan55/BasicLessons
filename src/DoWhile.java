cab298c720c061611fdf7fdb26bbdffdf86eccc7
mishgan55 <vorota.mogilev@gmail.com>
2023-05-19 16:52:48 +0300

    Do...While loop

---
+++ src/DoWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int value;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введи 5");
+            value=scanner.nextInt();
+
+        }while (value!=5);
+            System.out.println("Вы ввели 5");
+
+
+
+    }
+}
