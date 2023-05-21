4ff8596ca8204dda31fe1203c4624874d3d7fb85
mishgan55 <vorota.mogilev@gmail.com>
2023-05-21 23:39:15 +0300

    Switch

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int a=scanner.nextInt();
+        switch (a){
+            case 0 :
+                System.out.println("you are was born");
+                break;
+            case 6 :
+                System.out.println("you start going to school");
+                break;
+            case 16 :
+                System.out.println("You finished school");
+                break;
+            default:
+                System.out.println("you are "+a);
+        }
+    }
+}
