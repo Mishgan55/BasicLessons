6304707a00d8c09cf25a27637c0b5366bf06b523
mishgan55 <vorota.mogilev@gmail.com>
2023-05-23 01:10:05 +0300

    for...each and String in arrays

---
+++ src/Arrays_of_Strings.java
@@ -0,0 +1,19 @@
+public class Arrays_of_Strings {
+    public static void main(String[] args) {
+        String[] strings=new String[4];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i]="world war "+i;
+        }
+        for (String string: strings){
+            System.out.println(string);
+        }
+        System.out.println();
+
+        int[] number={1,3,4,5,6,8};
+        int sum=0;
+        for (int x:number){
+            sum=sum+x;
+        }
+        System.out.println("sum= "+sum);
+    }
+}
