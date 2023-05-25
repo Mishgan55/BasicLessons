23f8f8c350a549ec4c19678f4ad55feac3f1fd83
mishgan55 <vorota.mogilev@gmail.com>
2023-05-25 15:09:01 +0300

    Method void

--- src/basicLessons/ClassesAndObjects.java
+++ src/basicLessons/ClassesAndObjects.java
@@ -4,17 +4,29 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1=new Person();
         person1.name="Hanna";
-        person1.years=26;
-        System.out.println(person1.name+", "+person1.years);
+        person1.age=26;
+        person1.sayHello();
+        person1.sayNameAndAge();
+
 
         Person person2=new Person();
-        person2.name="Ksysha";
-        person2.years=9;
-        System.out.println(person2.name+", "+person2.years);
+        person2.name="Ksenia";
+        person2.age=9;
+        person2.sayHello();
+        person2.sayNameAndAge();
+
     }
 
 }
 class Person{
     String name;
-    int years;
+    int age;
+
+    void sayNameAndAge(){
+        System.out.println("My name is "+name+", I am "+age+" years old!");
+    }
+
+    void sayHello(){
+        System.out.println("Hello! ");
+    }
 }
