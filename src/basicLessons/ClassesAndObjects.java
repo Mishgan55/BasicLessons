ad618dd8df7852ff548922538095af950df34c0e
mishgan55 <vorota.mogilev@gmail.com>
2023-05-28 15:47:12 +0300

    Add method with parameters

--- src/basicLessons/ClassesAndObjects.java
+++ src/basicLessons/ClassesAndObjects.java
@@ -3,17 +3,18 @@ package basicLessons;
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1=new Person();
-        person1.name="Hanna";
-        person1.age=26;
+        person1.setNameAndAge("Hanna",25);
         person1.sayHello();
         person1.sayNameAndAge();
         int years1 = person1.calculateYearsToRetirement();
         System.out.println("Years to retirement: "+years1);
 
 
+        System.out.println();
+
+
         Person person2=new Person();
-        person2.name="Ksenia";
-        person2.age=9;
+        person2.setNameAndAge("Ksenia",9);
         person2.sayHello();
         person2.sayNameAndAge();
         int years2 = person2.calculateYearsToRetirement();
@@ -26,6 +27,11 @@ public class ClassesAndObjects {
 class Person{
     String name;
     int age;
+
+    void setNameAndAge(String userName, int userAge){
+        name=userName;
+        age=userAge;
+    }
 
     int calculateYearsToRetirement(){
         int years=65-age;
