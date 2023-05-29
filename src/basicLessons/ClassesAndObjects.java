5c44354e262fba5bea38941a6c9296608abdcba2
mishgan55 <vorota.mogilev@gmail.com>
2023-05-29 14:06:45 +0300

    Getters and Setters

--- src/basicLessons/ClassesAndObjects.java
+++ src/basicLessons/ClassesAndObjects.java
@@ -3,7 +3,8 @@ package basicLessons;
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1=new Person();
-        person1.setNameAndAge("Hanna",25);
+        person1.setName("Hanna");
+        person1.setAge(26);
         person1.sayHello();
         person1.sayNameAndAge();
         int years1 = person1.calculateYearsToRetirement();
@@ -14,7 +15,8 @@ public class ClassesAndObjects {
 
 
         Person person2=new Person();
-        person2.setNameAndAge("Ksenia",9);
+        person2.setName("");
+        person2.setAge(-2);
         person2.sayHello();
         person2.sayNameAndAge();
         int years2 = person2.calculateYearsToRetirement();
@@ -25,8 +27,31 @@ public class ClassesAndObjects {
 
 }
 class Person{
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName){
+        if (userName.isEmpty()){
+            System.out.println("You didn't write down name!");}
+        else {
+            name=userName;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int userAge){
+        if (userAge<0){
+            System.out.println("you wrote down wrong age!");
+        }else {
+            age=userAge;
+        }
+    }
+
+    public int getAge(){
+        return age;
+    }
 
     void setNameAndAge(String userName, int userAge){
         name=userName;
