e44d1ef4b4b0c36f387403a418f93719ff2cbbf3
mishgan55 <vorota.mogilev@gmail.com>
2023-06-01 16:41:57 +0300

    System.out.printf

---
+++ src/basicLessons/SOUTF.java
@@ -0,0 +1,9 @@
+package basicLessons;
+
+public class SOUTF {
+    public static void main(String[] args) {
+        System.out.printf("This is how to work with printf: %10.2f \n"  ,22.4563);
+        System.out.printf("This is how to work with printf: %2d \n",35673);
+        System.out.printf("This is how to work with printf: %-15s", "Hello java");
+    }
+}
