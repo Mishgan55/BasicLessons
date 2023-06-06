eb6d90aed6da01473da5e8523eef7b1f3026ee37
mishgan55 <vorota.mogilev@gmail.com>
2023-06-06 17:35:30 +0300

    Java Anagrams

--- src/hackerRank/HackerRankSolution15.java
+++ src/hackerRank/HackerRankSolution15.java
@@ -1,24 +1,39 @@
 package hackerRank;
+/*Two strings, a and b, are called anagrams if they contain all the same characters in the same frequencies.
+ For this challenge, the test is not case-sensitive.
+  For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
 
+ */
+
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class HackerRankSolution15 {
-    public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
+    static boolean isAnagram(String a, String b) {
+        String s = a.toLowerCase();
+        String s1 = b.toLowerCase();
+        if (a.length() != b.length()){
+            return false;
+        }
+
+        char[] chars = s.toCharArray();
+        char[] chars1 = s1.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chars1);
+
+        return Arrays.equals(chars,chars1);
 
 
-
-        return smallest + "\n" + largest;
     }
 
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        int k = scan.nextInt();
-        scan.close();
 
-        System.out.println(getSmallestAndLargest(s, k));
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 }
