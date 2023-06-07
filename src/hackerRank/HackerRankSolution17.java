2d16eb0199e25af6a797962831c0756a883cb0bf
mishgan55 <vorota.mogilev@gmail.com>
2023-06-07 17:19:31 +0300

    Pattern Syntax Checker

---
+++ src/hackerRank/HackerRankSolution17.java
@@ -0,0 +1,34 @@
+package hackerRank;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+/*Using Regex, we can easily match or search for patterns in a text.
+ Before searching for a pattern, we have to specify one using some well-defined syntax.
+
+In this problem, you are given a pattern. You have to check whether the syntax of the given pattern is valid.
+
+Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.*/
+
+public class HackerRankSolution17 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+       while (testCases>0) {
+           String pattern=in.nextLine();
+           try {
+               Pattern.compile(pattern);
+               System.out.println("Valid");
+           }catch (PatternSyntaxException ex){
+               System.out.println("Invalid");
+           }
+           testCases--;
+
+       }
+
+
+    }
+}
+
