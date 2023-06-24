640e27de764c8b440471fb7700e0f1e1b6543a9c
mishgan55 <vorota.mogilev@gmail.com>
2023-06-24 17:34:55 +0300

    Serializable2

--- src/basicLessons/serialize/ReadObject.java
+++ src/basicLessons/serialize/ReadObject.java
@@ -1,8 +1,11 @@
 package basicLessons.serialize;
 
+
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
@@ -10,10 +13,9 @@ public class ReadObject {
         FileInputStream fileInputStream = new FileInputStream("people.bin");
 
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            Person m =(Person) objectInputStream.readObject();
-            Person a = (Person) objectInputStream.readObject();
-            System.out.println(m);
-            System.out.println(a);
+            Person[] people = (Person[]) objectInputStream.readObject();
+            System.out.println(Arrays.toString(people));
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
--- src/basicLessons/serialize/WriteObject.java
+++ src/basicLessons/serialize/WriteObject.java
@@ -1,20 +1,21 @@
 package basicLessons.serialize;
 
-import java.io.FileNotFoundException;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class WriteObject {
     public static void main(String[] args) {
-        Person misha = new Person(1, "Misha");
-        Person anna = new Person(2, "Anna");
 
+
+        Person[] people={new Person(1, "Misha"),new Person(2, "Anna"),
+                new Person(3, "Ksenia")};
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("people.bin");
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(misha);
-            objectOutputStream.writeObject(anna);
+            objectOutputStream.writeObject(people);
+
             objectOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
